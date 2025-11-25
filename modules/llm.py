010be56ea29e559532495ec3a853fbad488f04b3
MilkyCat <opopopop50226@gmail.com>
2025-11-25 23:25:02 +0800

    localLLM

--- modules/llm.py
+++ modules/llm.py
@@ -19,24 +19,37 @@ def generate_response(prompt: str, model: str = "gemini-2.0-flash", web_search: 
         response = model.generate_content(prompt)
         return response.text
     
-    body = {
-        "provider": "HuggingFaceAPI",
-        "model": model,
-        # "stream": False,
-        "api_key": os.getenv("HUGGINGFACE_API_KEY"),     # 換成你的 HuggingFace Token
+    local_model = ["llama3.3:latest", "gemma3:4b", "gemma2:9b", "phi3:latest", "gemma3:27b", "gpt-oss:latest", "llama3.3:70b"]
+    if model in local_model:
+        def ask(prompt, model="gemma3:4b"):
+            url = "http://localhost:11434/api/generate"
+            payload = {
+                "model": model,
+                "prompt": prompt,
+                "stream": False
+            }
+            response = requests.post(url, json=payload)
+            return response.json().get("response")
+        return ask(prompt, model=model)
+    else:
+        body = {
+            "provider": "HuggingFaceAPI",
+            "model": model,
+            # "stream": False,
+            "api_key": os.getenv("HUGGINGFACE_API_KEY"),     # 換成你的 HuggingFace Token
 
-        "messages": [{"role": "user", "content": prompt}],
-        # g4f docker API 可能沒有 web_search 參數，如果你的版本支援可加
-        # "web_search": web_search  
-    }
+            "messages": [{"role": "user", "content": prompt}],
+            # g4f docker API 可能沒有 web_search 參數，如果你的版本支援可加
+            # "web_search": web_search  
+        }
 
-    response = requests.post(url, json=body)
-    response.raise_for_status()  # 若 HTTP 錯誤會丟例外
+        response = requests.post(url, json=body)
+        response.raise_for_status()  # 若 HTTP 錯誤會丟例外
 
-    choices = response.json().get("choices", [])
-    if not choices:
-        return ""  # 沒有回覆就回空字串
+        choices = response.json().get("choices", [])
+        if not choices:
+            return ""  # 沒有回覆就回空字串
 
-    # 取第一個 choice 的 content
-    return choices[0].get("message", {}).get("content", "")
+        # 取第一個 choice 的 content
+        return choices[0].get("message", {}).get("content", "")
 
