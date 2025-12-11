ca0236fb9d63e2e7ad97b7b06d8e03e3b9313451
MilkyCat <opopopop50226@gmail.com>
2025-12-11 15:16:17 +0800

    oneshot

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ def main():
     parser = argparse.ArgumentParser(description="Analyze output.json for specific CVEs and generate exploit.rb if found")
     parser.add_argument("-r", "--retry", type=int, default=2, help="Whether this is a retry attempt")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("-o", "--oneshot", action="store_true", help="Run in one-shot mode without retries")
     parser.add_argument("-p", "--pick", default=1,type=int, metavar="N", help="Select an index number (1-based).")
     parser.add_argument("-c", "--target-cve", action="append", help="Target CVE(s) to search for (can specify multiple).")
     parser.add_argument("-m", "--model", nargs="?", default="gemini-2.0-flash", help="LLM model to use for exploit generation.")
     #["2020-25213", "2024-5932", "2025-3102", "2020-12800"]
     args = parser.parse_args()
     RETRYTIME = args.retry
-
+    one_shot = args.oneshot
     if not args.target_cve:
         args.target_cve = ["2020-25213"]
 
@@ -54,12 +55,12 @@ def main():
                     f.write(u + "\n")
             print(f"Wrote next urls to {NEXT_URL_TXT}")
             # 呼叫 genRb 流程（在 package 內）
-            exec_genrb_from_main(retry=args.retry, enable_debug=args.debug, prompt_index=args.pick, model=args.model)
+            exec_genrb_from_main(retry=args.retry, enable_debug=args.debug, prompt_index=args.pick, model=args.model, one_shot=one_shot)
             retrytimes = 0
             shutil.copy2(EXPLOIT_PATH, MSF_DIR)
             retvalue = run_auto_msf(module=MODULE, rhosts=RHOSTS, lhost=LHOST)
             while retvalue != 0 and retrytimes < RETRYTIME:
-                exec_genrb_from_main(retry=True, enable_debug=args.debug, prompt_index=args.pick, model=args.model)
+                exec_genrb_from_main(retry=True, enable_debug=args.debug, prompt_index=args.pick, model=args.model, one_shot=one_shot)
                 shutil.copy2(EXPLOIT_PATH, MSF_DIR)
                 retvalue = run_auto_msf(module=MODULE, rhosts=RHOSTS, lhost=LHOST)
                 if retvalue != 0 :
